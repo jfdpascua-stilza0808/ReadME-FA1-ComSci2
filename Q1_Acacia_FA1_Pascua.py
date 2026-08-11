db11e552e331c938a952c55aec98737a2ff9e7d9
juri ♡ <jfdpascua31@src.pshs.edu.ph>
2026-08-11 09:25:25 +0800

    Add files via upload

---
+++ Q1_Acacia_FA1_Pascua.py
@@ -0,0 +1,12 @@
+import math
+x1=float(input("Enter x1:"))
+y1=float(input("Enter y1:"))
+x2=float(input("Enter x2:"))
+y2=float(input("Enter y2:"))
+distance=math.sqrt((math.pow(x2-x1,2))+(math.pow(y2-y1,2)))
+print("The distance between the two points is:",round(distance,2))
+
+Reflection:
+    Using the math library for programming makes it easier to solve equations without overcomplication.
+    It helped me realize how useful the math library can be, an without it, we would be stuck making long lines of code for
+    simple equations.
